dbe62fa07958ac11c72defe5eb4b8f6d439cc708
Danny <dhaynes@MM221591-PC.MITRE.ORG>
2018-03-15 13:12:25 -0400

    Fixed attribute descriptions.

--- controls/V-71943.rb
+++ controls/V-71943.rb
@@ -1,12 +1,12 @@
 UNSUCCESSFUL_ATTEMPTS = attribute('unsuccessful_attempts', default: '3',
-description: 'The scereensaver lock-delay must be less than or equal to the 
-specified value.')
+description: 'The account is denied access after the specified number of                                                                          
+consecutive failed logon attempts.')
 FAIL_INTERVAL = attribute('fail_interval', default: '900',
-description: 'The amount of time that an account must be locked out for after 
-the specified number of unsuccessful logon attempts.')
+description: 'The interval of time in which the consecutive failed logon                                                                          
+attempts must occur in order for the account to be locked out.')
 LOCKOUT_TIME = attribute('lockout_time', default: '604800',
-description: 'The amount of time that an account must be locked out for after
-the specified number of unsuccessful logon attempts.')
+description: 'The amount of time that an account must be locked out for                                                                           
+after the specified number of unsuccessful logon attempts.')
 
 # encoding: utf-8          
 #
